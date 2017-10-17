405ea9c5b8e546ce6d98daa72dcd50fec4370714
A M Corvi <amcorvi@icloud.com>
2017-10-17 12:51:56 -0400

    - Migration made over to feather from express for S&Gs...

--- src/index.js
+++ src/index.js
@@ -1,57 +1,13 @@
-// @flow
-'use strict'
+/* eslint-disable no-console */
+const logger = require('winston');
+const app = require('./app');
+const port = app.get('port');
+const server = app.listen(port);
 
-http://this.com
-import * as http from 'http';
-import debug from 'debug';
-import Api from './Api';
+process.on('unhandledRejection', (reason, p) =>
+  logger.error('Unhandled Rejection at: Promise ', p, reason)
+);
 
-// ErrnoError interface for use in onError
-declare interface ErrnoError extends Error {
-    errno?: number;
-    code?: string;
-    path?: string;
-    syscall?: string;
-}
-
-const logger = debug('flow-api:startup');
-const app: Api = new Api();
-const DEFAULT_PORT: number = 3000;
-const port: string | number = normalizePort(process.env.PORT);
-const server: http.Server = http.createServer(app.express);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val: any): number | string {
-    let port: string = (typeof val === 'string') ? parseInt(val, 10) : val;
-
-    if (port && isNaN(port)) return port;
-    else if (port >= 0) return port;
-    else return DEFAULT_PORT;
-}
-
-function onError(error: ErrnoError): void {
-    if (error.syscall !== 'listen') throw error;
-    let bind: string = (typeof port === 'string') ? `Pipe ${port}` : `Port ${port.toString()}`;
-
-    switch (error.code) {
-        case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-        case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-        break;
-        default:
-        throw error;
-    }
-}
-
-function onListening(): void {
-    let addr: string = server.address();
-    let bind: string = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-    logger(`Listening on ${bind}`);
-}
+server.on('listening', () =>
+  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+);
